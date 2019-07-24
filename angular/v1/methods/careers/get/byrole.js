da3e18a28156b638b6f270c7ef3674d06bc96796
PRIM\Kalyuzhnyy.Aleksey <Kalyuzhnyy.Aleksey@euroset.ru>
2019-07-24 14:24:18 +0300

    UPD catalog name

--- angular/v1/methods/careers/get/byrole.js
+++ angular/v1/methods/careers/get/byrole.js
@@ -7,7 +7,7 @@ function __main__() {
     var SQL = 'sql: ';
     SQL += 'SELECT crs.* FROM wt_flat.dbo.careers crs ';
     if ( role != 'admin' ) {
-      SQL += 'JOIN wt_flat.dbo.chareer_access crsacs ON crs.access = crsacs.id '
+      SQL += 'JOIN wt_flat.dbo.career_access crsacs ON crs.access = crsacs.id '
       SQL += "WHERE crsacs.name = 'byrole' AND crsacs.value = " + SqlLiteral( role );
     }
     var R = XQuery( SQL );
